dab46189da3ab4d4a8da5c4ba06fa5cef723889d
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 02:01:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,5 +1,7 @@
 require 'pry'
-contacts = {
+
+def remove_strawberry(contacts)
+  contacts = {
     "Jon Snow" => {
       name: "Jon",
       email: "jon_snow@thewall.we", 
@@ -10,11 +12,8 @@ contacts = {
       email: "freddy@mercury.com",
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
-}
+  }
 
-
-
-def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
