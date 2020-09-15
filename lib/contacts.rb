652fb64d4f847ac0bd7c6b515f0181c2877fedf4
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 02:05:29 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -13,7 +13,6 @@ def remove_strawberry(contacts)
     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   }
 }
-def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
@@ -22,7 +21,3 @@ def remove_strawberry(contacts)
         end
       end
     end
-contacts[:"Freddy Mercury"][:favorite_ice_cream_flavors]  
-end
-
-end
