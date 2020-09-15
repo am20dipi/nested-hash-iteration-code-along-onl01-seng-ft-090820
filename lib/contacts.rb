61a63ade0bc60f08d812b5f14c5edec1911a19f1
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-15 02:02:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def remove_strawberry(contacts)
-  def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
@@ -10,6 +9,6 @@ def remove_strawberry(contacts)
         end
       end
     end
-  end
 end
+
 
